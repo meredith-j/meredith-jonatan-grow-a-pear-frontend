5fa11fa9c6293da6369837c7dda4faa86369aae8
Meredith Jonatan <mjonatan@me.com>
2022-12-06 17:03:49 -0500

    finish initial header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
+import logo from '../../assets/images/GAP-logo.svg';
+import profileIcon from '../../assets/icons/profile-icon.svg'
 import './Header.scss'
+import { Navigate, NavLink } from 'react-router-dom';
 
 export default function Header() {
 
     return(
-        <>
-        <h1 className="header__heading">Grow a</h1>
-        <h1 className="header__heading">Pear</h1>
-        </>
+        <div className='header'>
+            <h1 className="header__heading">Grow a</h1>
+            <h1 className="header__heading">Pear</h1>
+            <img className="header__logo" src={logo} alt="a cute green pear winking at you" />
+            <NavLink to={"/profile"}><img className="header__profile-icon" src={profileIcon} /></NavLink>
+        </div>
     )
 }
