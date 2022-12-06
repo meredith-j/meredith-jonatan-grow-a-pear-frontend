458376d94269ac342f5fbc29d793966741f72712
Meredith Jonatan <mjonatan@me.com>
2022-12-06 16:41:46 -0500

    add fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+import Header from './components/Header/Header';
 import './App.scss';
 
 export default function App(){
 
   return(
     <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          < Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.scss'
+
+export default function Header() {
+
+    return(
+        <>
+        <h1 className="header__heading">Grow a</h1>
+        <h1 className="header__heading">Pear</h1>
+        </>
+    )
+}
