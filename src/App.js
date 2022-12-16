c8a555bcb737a70fbe1d899be1be7bb1a4a8dbe8
Meredith Jonatan <mjonatan@me.com>
2022-12-16 12:35:38 -0500

    final code clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import LandingPage from './pages/LandingPage/LandingPage';
-import UserProfilePage from './pages/UserProfilePage';
 import FormPage from './pages/FormPage/FormPage';
 import PlantOptionsPage from './pages/PlantOptionsPage/PlantOptionsPage'
 import Footer from './components/Footer';
@@ -17,7 +16,6 @@ export default function App(){
         <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/profile" element={<UserProfilePage />} />
           <Route path="/quiz" element={<FormPage />} />
           <Route path="/plants" element={<PlantOptionsPage />} />
         </Routes>
