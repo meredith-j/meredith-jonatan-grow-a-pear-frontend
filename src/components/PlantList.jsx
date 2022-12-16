0e791c97766ab7559b833b31ddc8673575d4b7c4
Meredith Jonatan <mjonatan@me.com>
2022-12-16 12:27:16 -0500

    add tablet landing page and updated plant list/options styles

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,9 +1,22 @@
+import plantImage from '../assets/images/raspberry-encore.jpeg'
 
-export default function PlantList () {
+export default function PlantList ({plants}) {
 
     return(
-        <>
+        <div className="list">
         <h2>Your Plant List</h2>
-        </>
+        {plants.map((plant) => {
+            return(
+                <div key={plant.id} className="list__card">
+                    <img className="list__image" src={plant.image} alt={plant.name} />
+                    <div className='list__details'>
+                        <h5 className='list__item'>{plant.name}</h5>
+                        <p>{plant.info}</p>
+                    </div>
+                </div>
+            )
+        })}
+        <button className='list__submit'>Confirm My List</button>
+        </div>
     )
 }
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,21 @@
 import Tagline from '../../components/Tagline'
 import ProductDetails from "../../components/ProductDetails";
+import TaglineTablet from '../../components/TaglineTablet'
+import ProductDetailsTablet from "../../components/ProductDetailsTablet";
 import './LandingPage.scss';
 
 export default function LandingPage() {
 
     return(
         <>
-        <Tagline />
-        <ProductDetails />
+            <div className='landing__mobile'>
+                <Tagline />
+                <ProductDetails />
+            </div>
+            <div className='landing__tablet'>
+                <TaglineTablet />
+                <ProductDetailsTablet />
+            </div>
         </>
     )
 }
